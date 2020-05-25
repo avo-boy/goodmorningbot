a4fc598418a70347bf230c762b790d7c114b03bb
avo-boy <lukevrensburg@gmail.com>
2020-05-25 15:17:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var greetings = ['Good morning','How did you sleep','Early bird catches the worm','"Good morning"',"Fixed your sleeping habit I see","Hey! Oh it's just you","Gaming time","Brush your teeth dammit"];
-var sadSpoon = ['','','',''];
+var greetings = ['Good morning', 'How did you sleep', 'Early bird catches the worm', '"_Good morning_"', "Fixed your sleeping habit I see", "Hey! Oh it's just you", "Gaming time", "Brush your teeth dammit"];
+var sadSpoon = ['Sorry if I hurt your feelings', 'Hey! Well done for being up', 'Sssh! The baby might still be sleeping, oh wait. You are the baby', 'uwu sowwi 4 hurtin ur feelings uwu'];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -13,20 +13,23 @@ client.on('message', msg => {
     if (msg.channel.name === 'goodmornin-chat') {
       var msgContent = msg.content.toLowerCase();
       if (msgContent == "good morning" || msgContent == "goodmornin" || msgContent == "goodmorning" || msgContent == "morning" || msgContent == "mornin" || msgContent == "gmornin" || msgContent == "gm") {
-        const channel = client.channels.cache.get("711852497408360578");
+        const channel = msg.channel;
         var r = Math.floor(Math.random() * greetings.length);
 
-        if (msg.author.id = "221604543828459520") {
+        if (msg.author.id == "221604543828459520") {
           var r2 = Math.random();
-          if (r2 <0.8) {
-            channel.send(`${sadSpoon}, ${msg.author}`);
-          } else{
+          if (r2 < 0.8) {
+            var r3 = Math.floor(Math.random() * sadSpoon.length);
+            channel.send(`${sadSpoon[r3]}, ${msg.author}`);
+          } else {
             channel.send(`${greetings[r]}, ${msg.author}`);
           }
-
         } else {
           channel.send(`${greetings[r]}, ${msg.author}`);
         }
+      } else if (msgContent == "ty" && msg.author.id == "221604543828459520"){
+        const channel = msg.channel;
+        channel.send(`Fuck you, ${msg.author}`);
       }
     }
   }
